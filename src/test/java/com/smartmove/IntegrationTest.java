f52105e030821404d97a062bc224705e8ce38c34
Danijel0 <dmiokovic1@etf.unsa.ba>
2026-02-18 16:08:13 +0100

    added jacoco coverage report for sonarqube analysis

---
+++ src/test/java/com/smartmove/IntegrationTest.java
@@ -0,0 +1,11 @@
+package com.smartmove;
+
+import org.junit.jupiter.api.Test;
+
+class IntegrationTest {
+    @Test
+    void runMainDemo() throws Exception {
+        // Runs all scenarios in Main.java
+        Main.main(new String[]{});
+    }
+}
